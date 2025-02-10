a988fce9ba8ebbb8894d97b6700708c7df2c2904
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-02-10 14:19:31 -0700

    Create feburary10.cs

---
+++ feburary10.cs
@@ -0,0 +1,7 @@
+// This is my journal prompt for feburary tenth
+Console.WriteLine(" Chappell Roan is the best");
+Console.WriteLine(" Congratulations!");
+Console.Write(" ");
+Console.WriteLine(" You wrote your first lines of code");
+Console.WriteLine("This is the first line");
+Console.Write("this is the second line");
