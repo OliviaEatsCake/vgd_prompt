fa785347af3002eb37723dde01e93005c35849ee
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-02-12 09:32:00 -0700

    Create feburary12.cs

---
+++ feburary12.cs
@@ -0,0 +1,16 @@
+//This is my code for february 12
+//I learned about the different literal data types, and the differences between them. 
+//I liked writing the pieces of code and making words.
+// I don't like that I keep forgetting to put the semi colon after the line of code, and its kind of annoying to add.  
+Console.WriteLine('b');
+Console.WriteLine("hello world");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
