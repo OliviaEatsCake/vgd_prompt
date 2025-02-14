6ce1df1f3ba508db7327971a5f9e7e5b632cd996
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-02-14 10:07:18 -0700

    Create febuary14.cs

---
+++ febuary14.cs
@@ -0,0 +1,8 @@
+//this is my code for feburary 14
+//I learned about variables and how to declare them
+
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
