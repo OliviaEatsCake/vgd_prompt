95c085244f19023a9c2ac8bb9262947b9c887c65
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-02-19 08:50:33 -0700

    Create feburary19.cs

---
+++ feburary19.cs
@@ -0,0 +1,14 @@
+//this is my code for february 19
+//I learned how to initialize, assign and retrieve variables
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine (firstName);
+firstName = "liam";
+Console.WriteLine (firstName);
+firstName = "Isabella";
+Console.WriteLine (firstName);
+firstName = "Yasmin";
+Console.WriteLine (firstName);
+string firstName = "Bob";
+Console.WriteLine(firstName);
