700909b1ecdef6e5add038f9993a1c3718e5b105
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-02-24 14:12:50 -0700

    Create feburary24.cs

---
+++ feburary24.cs
@@ -0,0 +1,14 @@
+//this is my code for feburary 24
+//I learned how to use implicitly typed variables, and use the var keyword
+//I also learned how to put everything together to create a sentence with both variables and literal values
+var recipient = "Bob";
+var emailNumber = 3;
+var temp = 34.4;
+Console.Write("Hello, ");
+Console.Write(recipient);
+Console.Write("!");
+Console.Write(" You have ");
+Console.Write(emailNumber);
+Console.Write( " messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" degrees celsius." ); 
