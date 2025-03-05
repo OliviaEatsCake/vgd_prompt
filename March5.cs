1dfda6e1c387fc6b6e68ac84f6310186a6574e1a
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-03-05 09:21:16 -0700

    Create March5.cs

---
+++ March5.cs
@@ -0,0 +1,10 @@
+//today I learned how to print literal values, and went over the main types of data
+//this was my code for March 5 
+
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
