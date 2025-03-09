3797fab7754cec63290f062926a2810626a6de94
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-03-08 17:31:38 -0700

    Create march7.cs

---
+++ march8,missedmarch7.cs
@@ -0,0 +1,23 @@
+//I learned how to use variable tyes, there uses functions and how to write them
+// this is my lesson for march 7, I missed it so I had to do this march 8, I think I may have done this before, but I am not sure. 
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine (firstName);
+firstName = "Isabella";
+Console.WriteLine (firstName);
+firstName = "Yasmin";
+Console.WriteLine (firstName);
+var message = "Hello world!";
+var name = "Bob";
+var inbox = 3;
+var temp = 34.4;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You Have ");
+Console.Write(inbox);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
