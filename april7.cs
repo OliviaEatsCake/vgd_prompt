5e2f0ce410aaf63a3d30825ba8515506e4c54312
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-04-07 13:47:59 -0600

    Create april7.cs

---
+++ april7.cs
@@ -0,0 +1,8 @@
+//this is my code for April 7
+//I learned the basics of addition and how adding a bracket changes the equation. I also learned about overloaded variables. 
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+string firstName ="Chappell Roan";
+int wigetsSold = 7;
+Console.WriteLine(firstName + " sold " + (wigetsSold + 7) +" widgets ");
