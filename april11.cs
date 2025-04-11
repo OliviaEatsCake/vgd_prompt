0af459f77941c16cd3546828825deb482e9b6562
OliviaEatsCake <OliviaS533@StemCollegiate.ca>
2025-04-11 10:18:56 -0600

    Create april11.cs

---
+++ april11.cs
@@ -0,0 +1,8 @@
+//this is my code for april 11
+//I tested my ability to use the math in coding
+int fahrenheit = 94;
+int firstStep = (fahrenheit - 32);
+decimal secondStep = firstStep * 5/9m;
+Console.Write(" The temperature is ");
+Console.Write(secondStep);
+Console.Write(" celsius.");
